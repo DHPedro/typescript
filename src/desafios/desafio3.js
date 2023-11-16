230483696417335b99918c5ddcb36f731be4efde
DH_Pedro <98625506+DHPedro@users.noreply.github.com>
2023-11-16 17:41:05 -0300

    Update desafio3.js

--- src/desafios/desafio3.js
+++ src/desafios/desafio3.js
@@ -1,31 +1,34 @@
-// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLButtonElement | null;
+let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement | null;
+let campoSaldo = document.getElementById('campo-saldo') as HTMLDivElement | null;
 
-let botaoAtualizar = document.getElementById('atualizar-saldo');
-let botaoLimpar = document.getElementById('limpar-saldo');
-let soma = document.getElementById('soma');
-let campoSaldo = document.getElementById('campo-saldo');
+if (campoSaldo) {
+  campoSaldo.innerHTML = '0';
+}
 
-campoSaldo.innerHTML = 0
-
-function somarAoSaldo(soma) {
-    campoSaldo.innerHTML += soma;
+function somarAoSaldo(valor: string | number) {
+  if (campoSaldo) {
+    campoSaldo.innerHTML = (parseInt(campoSaldo.innerHTML) + parseInt(valor.toString())).toString();
+  }
 }
 
 function limparSaldo() {
+  if (campoSaldo) {
     campoSaldo.innerHTML = '';
+  }
 }
 
-botaoAtualizar.addEventListener('click', function () {
-    somarAoSaldo(soma.value);
-});
+if (botaoAtualizar) {
+  botaoAtualizar.addEventListener('click', function () {
+    let soma = document.getElementById('soma') as HTMLInputElement | null;
+    if (soma && campoSaldo) {
+      somarAoSaldo(soma.value);
+    }
+  });
+}
 
-botaoLimpar.addEventListener('click', function () {
+if (botaoLimpar) {
+  botaoLimpar.addEventListener('click', function () {
     limparSaldo();
-});
-
-/**
-    <h4>Valor a ser adicionado: <input id="soma"> </h4>
-    <button id="atualizar-saldo">Atualizar saldo</button>
-    <button id="limpar-saldo">Limpar seu saldo</button>
-    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
- */
+  });
+}
